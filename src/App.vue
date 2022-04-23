9ce13d77be51e468ee600ebce02da0d67b28890a
heochristopher <71462915+heochristopher@users.noreply.github.com>
2022-04-23 12:42:12 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <Particles id="tsparticles" :options="{
+      fpsLimit: 60,
+      particles: {
+        color: {
+          value: '#000'
+        },
+        move: {
+          enable: true
+        },
+        links: {
+          enable: true,
+          color: '#000'
+        }
+      }
+    }" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#tsparticles {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
